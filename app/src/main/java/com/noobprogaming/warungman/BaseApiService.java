0ef475a38345089d2adbbee544c65d3d04b47d48
alvinbintang <alvinrr91@gmail.com>
2019-12-12 18:00:51 +0700

    Add BaseAPIService

---
+++ app/src/main/java/com/noobprogaming/warungman/BaseApiService.java
@@ -0,0 +1,17 @@
+package com.noobprogaming.warungman;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface BaseApiService {
+    @FormUrlEncoded
+    @POST("login")
+    Call<ResponseBody> LoginRequest (@Field("email") String email, @Field("password") String password);
+
+    @FormUrlEncoded
+    @POST("register")
+    Call<ResponseBody> RegisterRequest (@Field("nama") String nama, @Field("email") String email, @Field("password") String password);
+}
