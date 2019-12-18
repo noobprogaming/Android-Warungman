0bb3a7a68c703627a9b67ebded71cf808cbbd8c3
DinaAmalia <dina.26@students.amikom.ac.id>
2019-12-18 12:23:50 +0700

    ok

--- app/src/main/java/com/noobprogaming/warungman/BaseApiService.java
+++ app/src/main/java/com/noobprogaming/warungman/BaseApiService.java
@@ -4,7 +4,10 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface BaseApiService  {
 
@@ -18,5 +21,27 @@ public interface BaseApiService  {
     Call<ResponseBody> registerRequest(@Field("nama") String nama,
                                        @Field("email") String email,
                                        @Field("password") String password);
+    @FormUrlEncoded
+    @POST("cancelOrder")
+    Call<ResponseBody> cancelOrderRequest (@Header("Authorization") String token,
+                                           @Field("purchase_id") String purchase_id);
+
+@FormUrlEncoded
+    @POST("storePayment")
+    Call<ResponseBody> storePaymentRequest (@Header("Authorization") String token,
+                                            @Field("qr_code") String qr_code,
+                                            @Field("purchase_id") String purchase_id,
+                                            @Field("note") String note,
+                                            @Field("total_price") String total_price);
+@FormUrlEncoded
+    @POST("storeCart")
+    Call<ResponseBody> storeCartRequest (@Header("Authorization") String token,
+                                         @Field("seller_id") String seller_id,
+                                         @Field("item_id") String item_id,
+                                         @Field("amount")String amount);
+@GET("itemDetail/{item_id}")
+    Call<ResponseBody> itemDetailRequest (@Header("Authorization") String token,
+                                          @Path("item_id") String item_id);
+
 
 }
